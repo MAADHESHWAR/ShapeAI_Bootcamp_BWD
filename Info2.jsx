1cd3ff8386d7a32bf4de451ef01569c7c346d41a
MAADHESHWAR <86545642+MAADHESHWAR@users.noreply.github.com>
2021-06-27 12:52:55 +0530

    Add files via upload

---
+++ Info2.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info2(){
+  return(
+    <div className="note">
+    <h1>Python network security</h1>
+    <p> basic network securty in Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info2;
---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+  return(
+    <div className="note">
+    <h1>FOR GETTING CERTIFICATE</h1>
+    <p> IT IS MANDATORY TO COMPLETE THE FINAL PROJECT</p>
+    </div>
+  );
+}
+
+export default Note;
